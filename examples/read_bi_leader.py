d44ae574af87fffb8175d816e9f036bf955f9398
LEARNING DESKTOP <zhamers@ethrobotics.ch>
2025-11-29 13:43:43 +0100

    added some debug prints

--- examples/read_bi_leader.py
+++ examples/read_bi_leader.py
@@ -11,10 +11,19 @@ bi_leader.connect()
 
 freq = 200  # Hz
 
+start_time = time.time()
+count = 0
 try:
     while True:
+        count += 1
         bi_leader_action = bi_leader.get_action()
-        print({key: f"{value:.2f}" for key, value in bi_leader_action.items()})
+        # print({key: f"{value:.2f}" for key, value in bi_leader_action.items()})
         time.sleep(1 / freq)
+        time_elapsed = time.time() - start_time
+        if count % 400 == 0:
+            print(f"elapsed time iterations: {time_elapsed:.6f} seconds")
+        if time_elapsed >= 0.1:
+            print(f"Max elapsed time larger then 100ms: {time_elapsed:.2f} seconds")
+        start_time = time.time()
 except KeyboardInterrupt:
     print("\nStopping reading...")
