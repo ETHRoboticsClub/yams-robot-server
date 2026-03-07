be7e1fbddd4405bdbaa547b3eec137df360e2259
mira <michael.razoumovitch@gmail.com>
2026-03-07 17:37:25 +0100

    fixed naming

--- src/lerobot_camera_cached/camera_opencv_cached.py
+++ src/lerobot_camera_cached/camera_opencv_cached.py
@@ -1,8 +1,13 @@
-from .camera_opencv_cached import OpenCVCameraConfigCached
+from .cached_config import OpenCVCameraConfigCached
+from lerobot.cameras.opencv.camera_opencv import OpenCVCamera
 from numpy.typing import NDArray  # type: ignore  # TODO: add type stubs for numpy.typing
 from typing import Any
 
-class OpenCVCameraCached(OpenCVCameraConfigCached):
+class OpenCVCameraCached(OpenCVCamera):
+    def __init__(self, config: OpenCVCameraConfigCached):
+        super().__init__(config)
+        self.config = config
+
     def async_read(self, timeout_ms: float = 200) -> NDArray[Any]:
         with self.frame_lock:
             frame = self.latest_frame
