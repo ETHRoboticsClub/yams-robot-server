391e075088b06e59266c63f492b8d649b0f643dc
mira <michael.razoumovitch@gmail.com>
2026-03-07 17:33:04 +0100

    bug fix

--- src/lerobot_camera_cached/cached_config.py
+++ src/lerobot_camera_cached/cached_config.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+from lerobot.cameras.configs import CameraConfig
 from lerobot.cameras.opencv.configuration_opencv import OpenCVCameraConfig
 
 @CameraConfig.register_subclass("opencv-cached")
