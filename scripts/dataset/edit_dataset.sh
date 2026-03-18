131006d919d2a6a1414f9f91cc3dfdf0a73d8a7b
baraql <baraqlip@gmail.com>
2026-03-18 16:40:39 +0100

    add editing files

---
+++ scripts/dataset/edit_dataset.sh
@@ -0,0 +1,5 @@
+uv run lerobot-edit-dataset \
+    --repo_id ETHRC/towelspring26 \
+    --operation.type delete_episodes \
+    --new_repo_id ETHRC/towelspring26-cleaned \
+    --operation.episode_indices "[17, 19, 26, 27, 29, 46, 47, 65, 106, 108, 113, 137, 138]"
---
+++ scripts/dataset/trim_before_j2_lift.py
@@ -0,0 +1,22 @@
+import numpy as np
+from lerobot.datasets.lerobot_dataset import LeRobotDataset
+
+repo_id = "ETHRC/towelspring26"
+seconds_before = 0.5
+threshold = 0.3
+
+ds = LeRobotDataset(repo_id)
+fps = ds.fps
+frames_before = round(seconds_before * fps)
+
+for ep in range(ds.num_episodes):
+    ep_start = ds.meta.episodes["dataset_from_index"][ep]
+    ep_end = ds.meta.episodes["dataset_to_index"][ep]
+
+    rows = [ds[i] for i in range(ep_start, ep_end)]
+
+    joint2 = np.array([row["observation.state"][1].item() for row in rows])
+    hit = np.flatnonzero(joint2 > threshold)
+
+    new_start = ep_start if len(hit) == 0 else max(ep_start, ep_start + int(hit[0]) - frames_before)
+    print(ep, ep_start, ep_end, "->", new_start, ep_end)
