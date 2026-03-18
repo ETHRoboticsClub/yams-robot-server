4b6589b0e9c789fc5a636952182a1fd205a826f0
baraql <baraqlip@gmail.com>
2026-03-18 16:53:34 +0100

    change system to user

--- scripts/dataset/trim_before_j2_lift.py
+++ scripts/dataset/trim_before_j2_lift.py
@@ -9,6 +9,8 @@ ds = LeRobotDataset(repo_id)
 fps = ds.fps
 frames_before = round(seconds_before * fps)
 
+print(f"num_episodes={ds.num_episodes}")
+
 for ep in range(ds.num_episodes):
     ep_start = ds.meta.episodes["dataset_from_index"][ep]
     ep_end = ds.meta.episodes["dataset_to_index"][ep]
@@ -19,4 +21,6 @@ for ep in range(ds.num_episodes):
     hit = np.flatnonzero(joint2 > threshold)
 
     new_start = ep_start if len(hit) == 0 else max(ep_start, ep_start + int(hit[0]) - frames_before)
-    print(ep, ep_start, ep_end, "->", new_start, ep_end)
+    print(f"ep {ep + 1}/{ds.num_episodes}: {ep} {ep_start} {ep_end} -> {new_start} {ep_end}")
+
+print("done")
